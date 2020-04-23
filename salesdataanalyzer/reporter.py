af8ccae2a6b83468be6998204d544f8b391c500b
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-04-22 21:28:56 -0300

    Add exception handling for missing keys in data summary dict

--- salesdataanalyzer/reporter.py
+++ salesdataanalyzer/reporter.py
@@ -13,14 +13,21 @@ class DataSummary(TypedDict):
 
 def write_report_file(file_name: str, data_summary: DataSummary) -> None:
     """Writes a report file in the output directory."""
-    report_text = REPORT_TEMPLATE.format(
-        customers_amount=data_summary['customers_amount'],
-        salesmen_amount=data_summary['salesmen_amount'],
-        most_expensive_sale_id=data_summary['most_expensive_sale_id'],
-        worst_salesman_name=data_summary['worst_salesman_name']
-    )
+    try:
+        report_text = REPORT_TEMPLATE.format(
+            customers_amount=data_summary['customers_amount'],
+            salesmen_amount=data_summary['salesmen_amount'],
+            most_expensive_sale_id=data_summary['most_expensive_sale_id'],
+            worst_salesman_name=data_summary['worst_salesman_name']
+        )
+    except KeyError as e:
+        raise MissingDataSummaryKeyError(f'{e.args[0]} in {data_summary}')
 
     report_file_path = OUTPUT_DIR_PATH / (file_name + REPORT_FILE_EXT)
     report_file_path.parent.mkdir(parents=True, exist_ok=True)
     with report_file_path.open(mode='w', encoding='utf-8') as report_file:
         report_file.write(report_text)
+
+
+class MissingDataSummaryKeyError(KeyError):
+    pass
