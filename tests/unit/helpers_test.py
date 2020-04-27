157435a67925df8ede5d396a9b6b5c15ef958dcc
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-04-27 10:10:40 -0300

    Add test for Customer class

--- tests/unit/helpers_test.py
+++ tests/unit/helpers_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from salesdataanalyzer.helpers import Salesman
+from salesdataanalyzer.helpers import Salesman, Customer
 
 
 class SalesmanTest(TestCase):
@@ -10,3 +10,12 @@ class SalesmanTest(TestCase):
         self.assertEqual('12312312312', salesman.cpf)
         self.assertEqual('John H. Patterson', salesman.name)
         self.assertEqual(192000.00, salesman.salary)
+
+
+class CustomerTest(TestCase):
+    def test_create_customer(self):
+        customer = Customer('12312312312312', 'Dale Carnegie', 'Farming')
+
+        self.assertEqual('12312312312312', customer.cnpj)
+        self.assertEqual('Dale Carnegie', customer.name)
+        self.assertEqual('Farming', customer.business_area)
