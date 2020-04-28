5df0609a6468585e5a420c3a4150774fad0ed9b1
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-04-28 15:36:42 -0300

    Add count_customers function

--- salesdataanalyzer/analyzer.py
+++ salesdataanalyzer/analyzer.py
@@ -1,5 +1,12 @@
-from salesdataanalyzer.helpers import Data, DataSummary
+from typing import List
+
+from salesdataanalyzer.helpers import Data, DataSummary, Customer
 
 
 def analyze_data(data: Data) -> DataSummary:
     """Returns a summary of the analyzed input data."""
+
+
+def count_customers(customers: List[Customer]) -> int:
+    """Returns the quantity of customers."""
+    return len(customers)
