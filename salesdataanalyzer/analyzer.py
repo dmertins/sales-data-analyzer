3e1c7389bde3328f4f669556f3b210d14b9109ff
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-04-29 15:52:46 -0300

    Add get_worst_salesman function

--- salesdataanalyzer/analyzer.py
+++ salesdataanalyzer/analyzer.py
@@ -34,3 +34,28 @@ def get_most_expensive_sale(sales: List[Sale]) -> Optional[Sale]:
             most_expensive_sale = sale
 
     return most_expensive_sale
+
+
+def get_worst_salesman(salesmen: List[Salesman],
+                       sales: List[Sale]) -> Optional[Salesman]:
+    """Returns the worst salesman."""
+    salesmen_total_values = {salesman.name: 0 for salesman in salesmen}
+
+    for sale in sales:
+        sale_total = 0
+
+        for sale_item in sale.items:
+            sale_total += sale_item.quantity * sale_item.price
+
+        salesmen_total_values[sale.salesman_name] = salesmen_total_values.get(
+            sale.salesman_name, 0) + sale_total
+
+    if not salesmen_total_values:
+        return None
+
+    worst_salesman_name = min(salesmen_total_values,
+                              key=salesmen_total_values.get)
+
+    for salesman in salesmen:
+        if salesman.name == worst_salesman_name:
+            return salesman
