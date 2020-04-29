d9c7282e3f313b2cafba882a205008f694f8a104
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-04-29 11:48:10 -0300

    Add get_most_expensive_sale function

--- salesdataanalyzer/analyzer.py
+++ salesdataanalyzer/analyzer.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional
 
-from salesdataanalyzer.helpers import Data, DataSummary, Customer, Salesman
+from salesdataanalyzer.helpers import Data, DataSummary, Customer, Salesman, \
+    Sale
 
 
 def analyze_data(data: Data) -> DataSummary:
@@ -15,3 +16,21 @@ def count_customers(customers: List[Customer]) -> int:
 def count_salesmen(salesmen: List[Salesman]) -> int:
     """Returns the quantity of salesmen."""
     return len(salesmen)
+
+
+def get_most_expensive_sale(sales: List[Sale]) -> Optional[Sale]:
+    """Returns the most expensive sale."""
+    most_expensive_sale = None
+    max_sale_total = 0
+
+    for sale in sales:
+        sale_total = 0
+
+        for sale_item in sale.items:
+            sale_total += sale_item.quantity * sale_item.price
+
+        if sale_total > max_sale_total:
+            max_sale_total = sale_total
+            most_expensive_sale = sale
+
+    return most_expensive_sale
