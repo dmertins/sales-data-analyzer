29ce2c7f2b632a2acbbd7498992089665ef641e0
Daniel Alves Mertins <dalvesmertins@gmail.com>
2020-05-01 17:24:35 -0300

    Add parser field separator configuration

--- salesdataanalyzer/settings.py
+++ salesdataanalyzer/settings.py
@@ -10,3 +10,5 @@ REPORT_TEMPLATE = 'customers amount: {customers_amount}\n'\
                   'salesmen amount: {salesmen_amount}\n' \
                   'most expensive sale id: {most_expensive_sale_id}\n' \
                   'worst salesman name: {worst_salesman_name}'
+
+FIELD_SEPARATOR = 'ç'
